8c57ac5796f998c223a6f0d049cf524ae1cdc43a
VictoriaSvyat <vickysvyatova@yandex.ru>
2023-09-08 16:39:54 +0300

    yes

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 void main()
 {   
-    printf("Hello, world!\n");
+    double a, b; 
+    char c;
+    printf("Write 2 num:\n");
+    scanf("%lf %lf", &a, &b);
+    printf("+    -\n");
+    scanf(" %c", &c);
+    if (c=='+')
+    {
+        printf("Their sum:\n");
+        printf("%lf\n", a + b);
+    }  
+    else
+    {
+        printf("Their dif:\n");
+        printf("%lf\n", a - b);  
+    }
+    printf("Have a nice day:)\n");
 }
